fc09fcfba99ae7c610366d51a8e70e1dd578e304
jinwenmao <jinwenmao@139.com>
2019-01-27 14:57:14 +0800

    page 327 test for app domain

---
+++ p32p/MyClassLibrary/MyClassLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyNamespace
+{
+    public class MyClass:MarshalByRefObject
+    {
+        public void TraceAppDomain()
+        {
+            AppDomain currentAppDomain;
+            currentAppDomain = AppDomain.CurrentDomain;
+            Console.WriteLine(currentAppDomain.FriendlyName);
+        }
+    }
+}
---
+++ p32p/test_appdomain/test_appdomain/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyNamespace;
+
+namespace test_appdomain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain currentAppDomain;
+            currentAppDomain = AppDomain.CurrentDomain;
+            Console.WriteLine(currentAppDomain.FriendlyName);
+
+            AppDomain newAppDomain;
+            newAppDomain = AppDomain.CreateDomain("My new Appdomain");
+            MyClass obj;
+            obj = (MyClass)newAppDomain.CreateInstanceAndUnwrap("MyClassLibrary", "MyNamespace.MyClass");
+            obj.TraceAppDomain();
+        }
+    }
+}
